01ad31b81ea9e4b5b0130ff04fbf55b9abf4897b
Paras Waghela <paraswaghela777@gmail.com>
2024-11-15 20:59:33 +0530

    * -> +

--- src/components/Step2.jsx
+++ src/components/Step2.jsx
@@ -8,7 +8,7 @@ function Step2({vis,setvis}){
     return(
         <div className="flex flex-col justify-center items-center gap-y-10">
             <p className='text-white text-xl w-[70%] text-center sm:w-full'>Double That Number !</p>
-            <p className='text-white text-xl w-[70%] text-center sm:w-full'>(Example : 2x2 = 4)</p>
+            <p className='text-white text-xl w-[70%] text-center sm:w-full'>(Example : 2 + 2 = 4)</p>
             <button className="bg-white p-2 text-black rounded-lg" onClick={submitHandler}>Next</button>
         </div>
     )
